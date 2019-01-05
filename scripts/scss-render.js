d76100b95630acc541df8d96e4914ad643db55b6
mulder21c <publisher@publisher.name>
2019-01-06 00:35:26 +0900

    scaffolding

---
+++ scripts/scss-render.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const sass = require("node-sass");
+const autoprefixer = require('autoprefixer');
+const postcss  = require('postcss');
+const path = require("path");
+const pkgConfig = require("../package.json");
+
+hexo.extend.renderer.register("scss", "css", (data, options) => {
+  const renderedCss = sass.renderSync({
+    data: data.text,
+    includePaths: [
+      path.join(__dirname, "../source/css"),
+    ],
+    outputStyle: "compressed",
+  });
+
+  const result = postcss([ autoprefixer({browsers: pkgConfig.browserslist}) ])
+  .process(renderedCss.css.toString(), { browsers: pkgConfig.browserslist })
+  .css;
+
+  return result;
+}, true);
---
+++ source/fonts/iropkebatang/README.md
@@ -0,0 +1,3 @@
+이롭게 바탕체
+
+출처: [iropke github](https://github.com/iropke/font-iropke-batang)
