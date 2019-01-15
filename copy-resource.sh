107596153e2adb47295edda02c01dd02512acf6b
mulder21c <publisher@publisher.name>
2019-01-15 11:59:47 +0900

    fix paths of resources using url_for for using subdirectory

--- copy-resource.sh
+++ copy-resource.sh
@@ -7,12 +7,12 @@ cp ./node_modules/webfontloader/webfontloader.js ./source/js/webfontloader.js
 # copy fontawesome fonts from node_modules
 cp -r ./node_modules/@fortawesome/fontawesome-free/webfonts/. ./source/fonts/fontawesome/
 # copy fontawesome css file and replace webfont font path
-sed 's/\.\.\/webfonts/\/fonts\/fontawesome/g' ./node_modules/@fortawesome/fontawesome-free/css/all.min.css > ./source/fonts/fontawesome/fontawesome.css
+sed 's/\.\.\/webfonts\///g' ./node_modules/@fortawesome/fontawesome-free/css/all.min.css > ./source/fonts/fontawesome/fontawesome.css
 
 # copy spoqa-han-sans fonts from node_modules
 cp -r ./node_modules/spoqa-han-sans/Subset/SpoqaHanSans/. ./source/fonts/spoqahansans/
-sed 's/\.\.\/Subset\/SpoqaHanSans/\/fonts\/spoqahansans/g' ./node_modules/spoqa-han-sans/css/SpoqaHanSans-kr.css > ./source/fonts/spoqahansans/SpoqaHanSans-kr.css
+sed 's/\.\.\/Subset\/SpoqaHanSans\///g' ./node_modules/spoqa-han-sans/css/SpoqaHanSans-kr.css > ./source/fonts/spoqahansans/SpoqaHanSans-kr.css
 
 # copy noto-sans-kr fonts from node_modules
 cp -r ./node_modules/typeface-noto-sans-kr/files/. ./source/fonts/notosanskr/
-sed 's/\.\/files/\/fonts\/notosanskr/g' ./node_modules/typeface-noto-sans-kr/index.css > ./source/fonts/notosanskr/NotoSansKR.css
+sed 's/\.\/files\///g' ./node_modules/typeface-noto-sans-kr/index.css > ./source/fonts/notosanskr/NotoSansKR.css
