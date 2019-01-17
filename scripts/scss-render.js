711df95b6a75d0e87ee130cbb1ebd85d1d3006e1
mulder21c <publisher@publisher.name>
2019-01-18 00:31:37 +0900

    create helper for post-specific scss

--- scripts/scss-render.js
+++ scripts/scss-render.js
@@ -5,6 +5,7 @@ const autoprefixer = require('autoprefixer');
 const postcss  = require('postcss');
 const path = require("path");
 const pkgConfig = require("../package.json");
+const fs = require("hexo-fs");
 
 hexo.extend.renderer.register("scss", "css", (data, options) => {
   const renderedCss = sass.renderSync({
@@ -21,3 +22,28 @@ hexo.extend.renderer.register("scss", "css", (data, options) => {
 
   return result;
 }, true);
+
+hexo.extend.helper.register('scss_post', (scss) => {
+  if(!scss) return ``;
+
+  const mixin = fs.readFileSync(path.join(__dirname, "../source/css/helpers/_mixin.scss"));
+  const functions = fs.readFileSync(path.join(__dirname, "../source/css/modules/_functions.scss"));
+  const variables = fs.readFileSync(path.join(__dirname, "../source/css/modules/_variables.scss"));
+
+  scss = `
+    ${mixin}
+    ${functions}
+    ${variables}
+  ` + scss;
+
+  const renderedCss = sass.renderSync({
+    data: scss,
+    outputStyle: "compressed",
+  });
+
+  const result = postcss([ autoprefixer({browsers: pkgConfig.browserslist}) ])
+  .process(renderedCss.css.toString(), { browsers: pkgConfig.browserslist })
+  .css;
+
+  return result;
+});
